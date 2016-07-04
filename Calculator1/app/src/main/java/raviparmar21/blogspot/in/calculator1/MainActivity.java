7e133d3b14d50e2cc8b359befa676fee70f610b6
parmarravi <parmarravi950@gmail.com>
2016-07-04 23:07:34 +0530

    added alert box

--- Calculator1/app/src/main/java/raviparmar21/blogspot/in/calculator1/MainActivity.java
+++ Calculator1/app/src/main/java/raviparmar21/blogspot/in/calculator1/MainActivity.java
@@ -1,19 +1,36 @@
 package raviparmar21.blogspot.in.calculator1;
 
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+    private static Button button_alert;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void onClickButton(View v) {
@@ -22,15 +39,91 @@ public class MainActivity extends AppCompatActivity {
         EditText n2 = (EditText) findViewById(R.id.NumField2);
         TextView result = (TextView) findViewById(R.id.Result);
 
+        assert n1 != null;
         int num1 = Integer.parseInt(n1.getText().toString());
+        assert n2 != null;
         int num2 = Integer.parseInt(n2.getText().toString());
         int sum = num1 + num2;
-        String sumresult=(Integer.toString(sum));// convert integer to string
+        String sumresult = (Integer.toString(sum));// convert integer to string
+        assert result != null;
         result.setText(sumresult);
 
         //Displaying Toast
-        Toast myToast=Toast.makeText(getApplicationContext(),sumresult , Toast.LENGTH_SHORT);
-        myToast.setGravity(Gravity.CENTER_HORIZONTAL,0,700);
+        Toast myToast = Toast.makeText(getApplicationContext(), sumresult, Toast.LENGTH_SHORT);
+        myToast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 700);
         myToast.show();
+    }
+
+    //Alert BOX
+    public void onButtonClickListener(View v) {
+        Button button_alert = (Button) findViewById(R.id.alertbox);
+        assert button_alert != null;
+        button_alert.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                        a_builder.setMessage("Do you want to close the App!!")
+                                .setCancelable(false)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                        AlertDialog alert = a_builder.create();
+                        alert.setTitle("Alert!!");
+                        alert.show();
+                    }
+                }
+        );
+    }
+//self Created
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://raviparmar21.blogspot.in.calculator1/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://raviparmar21.blogspot.in.calculator1/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
